20719cc928a8e7ac331e84debc9acee6532e62b3
LTD123456 <Lethienduc123@gmail.com>
2019-12-30 13:46:58 +0700

    crawl data

--- CrawlDataApp1/Program.cs
+++ CrawlDataApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,32 @@ namespace CrawlDataApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            HtmlWeb htmlWeb = new HtmlWeb()
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8  //Set UTF8 để hiển thị tiếng Việt
+            };
+
+            //Load trang web, nạp html vào document
+            HtmlDocument document = htmlWeb.Load("https://www.thegioididong.com/dtdd");
+
+            //Load các tag li trong tag ul
+            var threadItems = document.DocumentNode.SelectNodes("//ul[@class='homeproduct  ']/li");
+
+            var items = new List<object>();
+
+            foreach (var item in threadItems.ToList())
+            {
+                //Extract các giá trị từ các tag con của tag li
+                var linkNode = item.SelectSingleNode(".//a[contains(@href,'dtdd')]");
+                var link = "thegioididong.com"+linkNode.Attributes["href"].Value;
+                //var text = linkNode.InnerText;
+                var text = linkNode.SelectSingleNode(".//h3").InnerText;
+                //var readCount = item.SelectSingleNode(".//div[@class='folTypPost']/ul/li/b").InnerText;
+                var price = linkNode.SelectSingleNode(".//div[@class='price']/strong").InnerText;
+                //link = "thegioididong.com" + link;
+                items.Add(new { text, price, link });
+            }
         }
     }
 }
